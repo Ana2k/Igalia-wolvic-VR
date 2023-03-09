39fb560d46c772da84862d002be1a3b340c32df0
Ana2k <anushkachakra@gmail.com>
2023-03-10 00:57:32 +0530

    functioning color pallete with bubble drop. color pallette version1

--- app/src/common/shared/com/igalia/wolvic/ui/widgets/prompts/ColorPromptWidget.java
+++ app/src/common/shared/com/igalia/wolvic/ui/widgets/prompts/ColorPromptWidget.java
@@ -2,24 +2,16 @@ package com.igalia.wolvic.ui.widgets.prompts;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.lifecycle.LifecycleOwner;
 
 import com.igalia.wolvic.R;
 import com.igalia.wolvic.audio.AudioEngine;
-import com.skydoves.colorpickerview.AlphaTileView;
-import com.skydoves.colorpickerview.ColorEnvelope;
 import com.skydoves.colorpickerview.ColorPickerView;
-import com.skydoves.colorpickerview.flag.FlagView;
-import com.skydoves.colorpickerview.listeners.ColorListener;
-import com.skydoves.colorpickerview.listeners.ColorPickerViewListener;
-import com.skydoves.colorpickerview.sliders.AlphaSlideBar;
-import com.skydoves.colorpickerview.sliders.BrightnessSlideBar;
+import com.skydoves.colorpickerview.flag.BubbleFlag;
+import com.skydoves.colorpickerview.flag.FlagMode;
+import com.skydoves.colorpickerview.preference.ColorPickerPreferenceManager;
 
 public class ColorPromptWidget extends PromptWidget {
 
@@ -43,12 +35,13 @@ public class ColorPromptWidget extends PromptWidget {
 
     protected void initialize(Context aContext) {
         inflate(aContext, R.layout.prompt_color, this);
-
-        //implementing ColorPickerView
-        //check the colorPicker whether it is running the way we expect it to.
         new ColorPickerView.Builder(getContext()).build();
-        ColorPickerView colorPickerView =  findViewById(R.id.colorPickerView);
-        colorPickerView.setFlagView(new CustomFlag(getContext(), R.layout.layout_flag));
+
+        ColorPickerView colorPickerView = findViewById(R.id.colorPickerView);
+        BubbleFlag bubbleFlag = new BubbleFlag(getContext());
+        bubbleFlag.setFlagMode(FlagMode.ALWAYS);
+        colorPickerView.setFlagView(bubbleFlag);
+        ColorPickerPreferenceManager manager = ColorPickerPreferenceManager.getInstance(getContext());
 
         mAudio = AudioEngine.fromContext(aContext);
         mLayout = findViewById(R.id.layout);
@@ -62,6 +55,7 @@ public class ColorPromptWidget extends PromptWidget {
             }
             if (mPromptDelegate != null && mPromptDelegate instanceof ColorPromptDelegate) {
                 mPromptDelegate.dismiss();
+                manager.clearSavedAllData();
             }
             hide(REMOVE_WIDGET);
         });
@@ -71,8 +65,9 @@ public class ColorPromptWidget extends PromptWidget {
                 mAudio.playSound(AudioEngine.Sound.CLICK);
             }
             if (mPromptDelegate != null && mPromptDelegate instanceof ColorPromptDelegate) {
-
-                ((ColorPromptDelegate) mPromptDelegate).confirm("#FFBF00");
+                String colorCodeHex = "#" + colorPickerView.getColorEnvelope().getHexCode().substring(2, 8);
+                manager.saveColorPickerData(colorPickerView);
+                ((ColorPromptDelegate) mPromptDelegate).confirm(colorCodeHex);
             }
             hide(REMOVE_WIDGET);
         });
@@ -80,22 +75,5 @@ public class ColorPromptWidget extends PromptWidget {
 
     public interface ColorPromptDelegate extends PromptDelegate {
         void confirm(@NonNull final String color);
-    }
-    public class CustomFlag extends FlagView {
-
-        private TextView textView;
-        private AlphaTileView alphaTileView;
-
-        public CustomFlag(Context context, int layout) {
-            super(context, layout);
-            textView = findViewById(R.id.flag_color_code);
-            alphaTileView = findViewById(R.id.alpha_tile_view);
-        }
-
-        @Override
-        public void onRefresh(ColorEnvelope colorEnvelope) {
-            textView.setText("#" + colorEnvelope.getHexCode());
-            alphaTileView.setPaintColor(colorEnvelope.getColor());
-        }
     }
 }
